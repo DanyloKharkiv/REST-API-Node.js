584b6fa3502045385738c1138530ccd8551b9593
Danylo Kharkiv <donatelod12@gmail.com>
2023-06-05 22:41:27 +0300

    add MongoDB base url

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ const app = require('./app')
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
 })
+
+
+
+// BIhP6K5swm81XZiG
+const DBBASE = "mongodb+srv://danylo_kharkiv:BIhP6K5swm81XZiG@cluster0.pnhytyt.mongodb.net/?retryWrites=true&w=majority";
